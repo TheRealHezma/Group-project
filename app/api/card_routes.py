ad725b28a0583a74a3b0a11e19021f7c6c7aa4dd
Gavin Musgrove <gavin.musgrove87@gmail.com>
2024-07-31 18:37:11 -0500

    shuffled a route to the proper place

--- app/api/card_routes.py
+++ app/api/card_routes.py
@@ -4,35 +4,6 @@ from app.models import db, Card
 
 card_routes = Blueprint('cards', __name__)
 
-@card_routes.route('/lists/<int:list_id>/cards', methods=['POST'])
-@login_required
-def create_card(list_id):
-    """
-    Creates and returns a new Card
-    """
-    data = request.get_json()
-    title = data.get('title')
-    description = data.get('description')
-
-    if not title or not description:
-        errors = {}
-        if not title:
-            errors["title"] = "Title is required"
-        if not description:
-            errors["description"] = "Description is required"
-        return jsonify({"message": "Bad Request", "errors": errors}), 400
-
-    new_card = Card(
-        title=title,
-        list_id=list_id,
-        description=description
-    )
-
-    db.session.add(new_card)
-    db.session.commit()
-
-    return jsonify(new_card.to_dict()), 201
-
 @card_routes.route('/cards/<int:id>', methods=['PUT'])
 @login_required
 def edit_card(id):

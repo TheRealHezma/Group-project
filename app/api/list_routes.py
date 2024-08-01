5146f4252bc123375dc552c6503fae5ecdfae104
Hemza <hezmam6@gmail.com>
2024-08-01 08:00:18 -0400

    list routes tested and finished

--- app/api/list_routes.py
+++ app/api/list_routes.py
@@ -1,40 +1,31 @@
 from flask import Blueprint, jsonify, request
-from app.models import List, db
+from app.models import List, Card, db
 from flask_login import login_required
 
 list_routes = Blueprint('lists', __name__)
 
-# Get all Lists by a Board’s id
-@list_routes.route('/api/boards/<int:board_id>/lists', methods=['GET'])
-@login_required
-def get_lists_by_board(board_id):
-    """
-    Query for all lists associated with a board and returns them in a list of list dictionaries
-    """
-    lists = List.query.filter_by(board_id=board_id).all()
-    return {'lists': [list.to_dict() for list in lists]}, 200
 
 # Create a List
-@list_routes.route('/api/boards/<int:board_id>/lists', methods=['POST'])
-@login_required
-def create_list(board_id):
-    """
-    Create a new list and return the list in a dictionary
-    """
-    data = request.json
-    if 'name' not in data or not data['name']:
-        return jsonify({"message": "Bad Request", "errors": {"Name": "Name is required"}}), 400
+# @list_routes.route('/<int:board_id>/lists', methods=['POST'])
+# @login_required
+# def create_list(board_id):
+#     """
+#     Create a new list and return the list in a dictionary
+#     """
+#     data = request.json
+#     if 'name' not in data or not data['name']:
+#         return jsonify({"message": "Bad Request", "errors": {"Name": "Name is required"}}), 400
 
-    new_list = List(
-        name=data['name'],
-        board_id=board_id
-    )
-    db.session.add(new_list)
-    db.session.commit()
-    return new_list.to_dict(), 201
+#     new_list = List(
+#         name=data['name'],
+#         board_id=board_id
+#     )
+#     db.session.add(new_list)
+#     db.session.commit()
+#     return new_list.to_dict(), 201
 
 # Edit a List
-@list_routes.route('/api/lists/<int:id>', methods=['PUT'])
+@list_routes.route('/<int:id>', methods=['PUT'])
 @login_required
 def edit_list(id):
     """
@@ -54,13 +45,15 @@ def edit_list(id):
     return list_to_edit.to_dict(), 201
 
 # Delete a List
-@list_routes.route('/api/lists/<int:id>', methods=['DELETE'])
+@list_routes.route('/<int:id>', methods=['DELETE'])
 @login_required
 def delete_list(id):
     """
     Delete a list by id and return a success message
     """
+    print(id)
     list_to_delete = List.query.get(id)
+    print("HERE",list_to_delete)
     if not list_to_delete:
         return jsonify({"message": "List couldn't be found"}), 404
 
@@ -69,7 +62,7 @@ def delete_list(id):
     return jsonify({"message": "Successfully deleted"}), 200
 
 # Create a Card
-@list_routes.route('/lists/<int:list_id>/cards', methods=['POST'])
+@list_routes.route('/<int:list_id>/cards', methods=['POST'])
 @login_required
 def create_card(list_id):
     """
@@ -97,3 +90,17 @@ def create_card(list_id):
     db.session.commit()
 
     return jsonify(new_card.to_dict()), 201
+
+
+@list_routes.route('/<int:list_id>/cards', methods=['GET'])
+# @login_required
+def get_cards_by_list_id(list_id):
+    """
+    Get all Cards by List's ID
+    """
+    list_exist = List.query.get(list_id)
+    if not list_exist:
+        return jsonify({"message": "Cards do not exist because the list does not exist"}), 404
+
+    cards = Card.query.filter_by(list_id=list_id).all()
+    return jsonify({"Cards": [card.to_dict() for card in cards]}), 200

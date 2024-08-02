a5995ef9fee87228ee39adc701ea699205bb8917
stevenseb <stevenseb@hotmail.com>
2024-08-02 15:47:23 -0400

    run build files

--- react-vite/src/pages/Splash.jsx
+++ react-vite/src/pages/Splash.jsx
@@ -1,5 +1,5 @@
 // src/pages/Splash.js
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllBoards } from '../redux/board';
 import { Link } from 'react-router-dom';

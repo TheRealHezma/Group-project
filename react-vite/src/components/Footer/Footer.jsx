9cb5a5390fe05fa3ac4c1af1624c0b809cd0c413
Hemza <hezmam6@gmail.com>
2024-08-14 08:05:54 -0400

    fixed github links in footer. now they open a new tab

--- react-vite/src/components/Footer/Footer.jsx
+++ react-vite/src/components/Footer/Footer.jsx
@@ -6,12 +6,13 @@ const Footer = () => {
     return (
         <footer className="footer">
             <p>Contributors:</p>
-            {/* <p>Bobby Naluz I GitHub:<a href="https://github.com/bnaluz" rel='noopener'>bnaluz</a></p>
-            <p>Eric Stevens I GitHub:<a href="https://github.com/stevenseb" rel='noopener'>stevenseb</a></p>
-            <p>Gavin Musgrove I GitHub:<a href="https://github.com/GavinM404" rel='noopener'>GavinM404</a></p>
-            <p>Hemza Masour I GitHub:<a href="https://github.com/TheRealHezma" rel='noopener'>TheRealHezma</a></p> */}
-            <p>Hemza Mansour I Bobby Naluz I Eric Stevens I Gavin Musgrove</p>
-            <p><a href="https://github.com/TheRealHezma" rel='noopener'>TheRealHezma</a> I <a href="https://github.com/bnaluz" rel='noopener'>bnaluz</a> I <a href="https://github.com/stevenseb" rel='noopener'>stevenseb</a> I <a href="https://github.com/GavinM404" rel='noopener'>GavinM404</a></p>
+            <p>Hemza Mansour | Bobby Naluz | Eric Stevens | Gavin Musgrove</p>
+            <p>
+                <a href="https://github.com/TheRealHezma" target="_blank">TheRealHezma</a> |
+                <a href="https://github.com/bnaluz" target="_blank">bnaluz</a> |
+                <a href="https://github.com/stevenseb" target="_blank">stevenseb</a> |
+                <a href="https://github.com/GavinM404" target="_blank">GavinM404</a>
+            </p>
 
             <p>&copy; 2024 TaskWave. All rights reserved.</p>
         </footer>
